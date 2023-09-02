b37fcb91b7b48043cef1284cc5d861c09a5bcdfb
vanz1220 <ije.malong@gmail.com>
2023-09-02 19:05:50 +0800

    Changes

--- script/script.js
+++ script/script.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
      // owl carousel script
     $('.carousel').owlCarousel({
-        margin: 20,
+        margin: -30,
         loop: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
